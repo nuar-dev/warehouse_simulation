128063c4d93519360afe19f94832c55c58b47ebe
nuar <frank.streicher@nuar.de>
2025-06-08 16:35:45 +0200

    refactoring for logical structure v0.91

--- src/layout/components/AppNavbar.tsx
+++ src/layout/components/AppNavbar.tsx
@@ -9,7 +9,7 @@ import Typography from '@mui/material/Typography';
 import MenuRoundedIcon from '@mui/icons-material/MenuRounded';
 import DashboardRoundedIcon from '@mui/icons-material/DashboardRounded';
 import SideMenuMobile from './SideMenuMobile';
-import MenuButton from '../../components/ui/MenuButton';
+import MenuButton from '@/components/ui/MenuButton';
 import ColorModeIconDropdown from './ColorModeIconDropdown';
 
 const Toolbar = styled(MuiToolbar)({
--- src/main.tsx
+++ src/main.tsx
@@ -2,7 +2,7 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import { BrowserRouter } from "react-router-dom";
-import App from "./app/App";
+import App from "@/app/App";
 import CssBaseline from "@mui/material/CssBaseline";
 
 ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
